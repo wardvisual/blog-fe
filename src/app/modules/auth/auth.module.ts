2bfe3bdd5fe55a216dcdb40f8d8d863a2e3b62c4
wardvisual <wardvisual@gmail.com>
2023-09-10 01:34:10 +0800

    chore: setup files

---
+++ src/app/modules/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [CommonModule],
+})
+export class AuthModule {}
