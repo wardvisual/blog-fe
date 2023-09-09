8b4b05fe9e425f699599a9ac8ac0f7be8c9da224
wardvisual <wardvisual@gmail.com>
2023-09-10 01:52:11 +0800

    chore: setup modular route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './libs/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 
+const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./views/login/login.component').then((c) => c.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./views/register/register.component').then((c) => c.RegisterComponent),
+  },
+];
+
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [CommonModule],
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
 })
 export class AuthModule {}
