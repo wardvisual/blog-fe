0a189edd8f0793376e185bb62c75705f126e14a9
wardvisual <wardvisual@gmail.com>
2023-09-10 23:09:18 +0800

    move ng-zorro to core module

--- src/app/libs/shared/shared.module.ts
+++ src/app/libs/shared/shared.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import en from '@angular/common/locales/en';
-
-registerLocaleData(en);
-
 @NgModule({
-  imports: [BrowserAnimationsModule],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  imports: [],
+  exports: [],
 })
 export class SharedModule {}
