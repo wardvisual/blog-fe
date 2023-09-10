edf5d2ba6ae35a2e2be38c36e25140170812ca0b
wardvisual <wardvisual@gmail.com>
2023-09-10 20:40:34 +0800

    chore: create auth service

---
+++ src/app/features/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, from, map } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { LoginDto } from './dtos/login.dto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private readonly http: HttpClient) {}
+
+  login(user: LoginDto): Observable<any> {
+    return from(this.http.post(`${environment.apiUrl}/auth/login`, user)).pipe(
+      map((res: any) => res)
+    );
+  }
+}
