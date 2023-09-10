f9b5440a400c68bf89b418d3172c9e0f867976d7
wardvisual <wardvisual@gmail.com>
2023-09-10 23:14:26 +0800

    move ng-zorro to core module

--- src/app/libs/core/core.module.ts
+++ src/app/libs/core/core.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
+
 @NgModule({
-  imports: [HttpClientModule],
-  providers: [],
-  exports: [],
+  imports: [HttpClientModule, BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
 })
 export class CoreModule {}
