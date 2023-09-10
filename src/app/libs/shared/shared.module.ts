1b687ac588eccf2a571908b91b8e7f14e7c6abb6
wardvisual <wardvisual@gmail.com>
2023-09-10 23:05:29 +0800

    feat: setup ng-zorro-antd

--- src/app/libs/shared/shared.module.ts
+++ src/app/libs/shared/shared.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
+
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
 })
 export class SharedModule {}
