d2d22623f0ddef4aa8cac4458063ab88cd4c1b70
wardvisual <wardvisual@gmail.com>
2023-09-11 22:58:59 +0800

    feat: implement auth store

---
+++ src/app/features/auth/auth.store.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+import { Store } from '@/libs/shared/store/app.store';
+
+interface AuthState {
+  isAuthenticated?: boolean;
+  isSuccess?: boolean;
+  message?: string;
+  isLoading?: boolean;
+}
+
+const initialState: AuthState = {
+  isAuthenticated: false,
+  isSuccess: false,
+  message: '',
+  isLoading: false,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthStore extends Store<AuthState> {
+  constructor() {
+    super(initialState);
+  }
+
+  setState(authState: AuthState) {
+    this._setState((state) => ({
+      ...state,
+      ...authState,
+    }));
+  }
+}
