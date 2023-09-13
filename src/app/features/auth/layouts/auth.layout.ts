5fc0c9cc3322aa511ce12257fa3327db72385273
wardvisual <wardvisual@gmail.com>
2023-09-14 01:23:27 +0800

    chore: complete auth pages

--- src/app/features/auth/layouts/auth.layout.ts
+++ src/app/features/auth/layouts/auth.layout.ts
@@ -3,15 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'auth-layout',
   template: `
-    <header>
-      <!-- header content -->
-    </header>
-    <main class="h-full">
-      <router-outlet></router-outlet>
-    </main>
-    <footer>
-      <!-- footer content -->
-    </footer>
+    <div class="grid h-full place-items-center md:grid-cols-2">
+      <main class="max-w-lg p-5 text-start md:px-8">
+        <router-outlet></router-outlet>
+      </main>
+      <div class="hidden md:w-full md:block md:h-screen md:bg-blue-500"></div>
+    </div>
   `,
 })
 export class AuthLayout {}
--- src/app/features/auth/views/register/register.component.ts
+++ src/app/features/auth/views/register/register.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from '@/features/auth/auth.service';
+import { LoginDto } from '@/features/auth/dtos/login.dto';
+
+import { AuthStore } from '@/features/auth/auth.store';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  encapsulation: ViewEncapsulation.None,
 })
 export class RegisterComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private readonly authService: AuthService,
+    public readonly store: AuthStore
+  ) {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup<LoginDto>({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  get field(): any {
+    return {
+      username: this.loginForm.get('username'),
+      password: this.loginForm.get('password'),
+    };
+  }
+
+  login(event: Event) {
+    event.preventDefault();
+
+    const user = this.loginForm.getRawValue();
+
+    this.store.setState({ isLoading: true });
+
+    this.authService.login(user).subscribe(
+      (res) => {
+        this.store.setState({
+          isAuthenticated: true,
+          message: res.message,
+          isSuccess: true,
+          isLoading: false,
+        });
+
+        this.authService.setToken(res.data.accessToken);
+      },
+      ({ error }) => {
+        this.store.setState({
+          isAuthenticated: false,
+          message: error.message,
+          isSuccess: false,
+          isLoading: false,
+        });
+      }
+    );
+  }
 }
