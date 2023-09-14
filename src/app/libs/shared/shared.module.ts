d7f73b77bdc54d2ff7f0a320c5f4db71b6265827
wardvisual <wardvisual@gmail.com>
2023-09-14 13:29:49 +0800

    chore:  import and export module and components

--- src/app/libs/shared/shared.module.ts
+++ src/app/libs/shared/shared.module.ts
@@ -1,8 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+import { InputComponent } from '@/libs/shared/components';
+
+const modules = [
+  CommonModule,
+  ReactiveFormsModule,
+  NzAlertModule,
+  NzInputModule,
+  NzButtonModule,
+];
+
+const components = [InputComponent]
 
 @NgModule({
-  imports: [],
-  exports: [],
-  declarations: [],
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...modules, ...components],
 })
 export class SharedModule {}
