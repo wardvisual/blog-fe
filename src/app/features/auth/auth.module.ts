06fbc726b9db1232cec866ee78c661f92a49dd8f
wardvisual <wardvisual@gmail.com>
2023-09-14 13:31:42 +0800

    chore: remove shareable modules and components and import shared module

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,17 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { NzAlertModule } from 'ng-zorro-antd/alert';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzButtonModule } from 'ng-zorro-antd/button';
 
 import { SharedModule } from '@/libs/shared/shared.module';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 import { AuthLayout } from './layouts/auth.layout';
-import { InputComponent } from '@/libs/shared/components/input/input.component';
 
 const routes: Routes = [
   {
@@ -31,18 +24,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AuthLayout, LoginComponent, RegisterComponent, InputComponent],
-  imports: [
-    SharedModule,
-    ReactiveFormsModule,
-    CommonModule,
-
-    NzAlertModule,
-    NzInputModule,
-    NzButtonModule,
-
-    RouterModule.forChild(routes),
-  ],
-  exports: [CommonModule, RouterModule, ReactiveFormsModule],
+  declarations: [AuthLayout, LoginComponent, RegisterComponent],
+  imports: [SharedModule, RouterModule.forChild(routes)],
 })
 export class AuthModule {}
