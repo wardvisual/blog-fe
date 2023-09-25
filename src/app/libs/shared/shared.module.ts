5b5e9b166264083003b97e3cf9ebe6e17b312a89
wardvisual <wardvisual@gmail.com>
2023-09-25 22:18:31 +0800

    chore: import several components

--- src/app/libs/shared/shared.module.ts
+++ src/app/libs/shared/shared.module.ts
@@ -5,6 +5,9 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 import { InputComponent } from '@/libs/shared/components';
 
@@ -14,9 +17,12 @@ const modules = [
   NzAlertModule,
   NzInputModule,
   NzButtonModule,
+  NzCardModule,
+  NzAvatarModule,
+  NzIconModule,
 ];
 
-const components = [InputComponent]
+const components = [InputComponent];
 
 @NgModule({
   declarations: [...components],
