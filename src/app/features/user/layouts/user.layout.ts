b5855a050b5c15c43dc5e1d849f84c78986e632b
wardvisual <wardvisual@gmail.com>
2023-09-25 22:15:20 +0800

    chore: add max with

--- src/app/features/user/layouts/user.layout.ts
+++ src/app/features/user/layouts/user.layout.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user-layout',
   template: `
-    <div class="mx-auto container">
+    <div class="mx-auto container max-w-[800px]">
       <main>
         <router-outlet></router-outlet>
       </main>
