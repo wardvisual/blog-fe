7ef62686bcca039c0ddd28152ce7265a9865c2ac
wardvisual <wardvisual@gmail.com>
2023-09-30 11:11:23 +0800

    fix: auth path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,20 @@ import { PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'auth',
     loadChildren: () =>
       import('./features/auth/auth.module').then((m) => m.AuthModule),
-  },
-  {
-    path: '',
-    loadChildren: () =>
-      import('./features/posts/posts.module').then((m) => m.PostsModule),
   },
   {
     path: 'feed',
     pathMatch: 'full',
     loadChildren: () =>
       import('./features/user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/posts/posts.module').then((m) => m.PostsModule),
   },
 ];
 
