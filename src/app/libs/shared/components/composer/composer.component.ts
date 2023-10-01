81589288c0133e2850f5c13aec5f8fb6f019fbc9
wardvisual <wardvisual@gmail.com>
2023-10-01 21:23:13 +0800

    chore: handle image file upload

--- src/app/libs/shared/components/composer/composer.component.ts
+++ src/app/libs/shared/components/composer/composer.component.ts
@@ -1,15 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
-import { BlockType } from '@/libs/shared/interfaces';
+// import { BlockType } from '@/libs/shared/interfaces';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
+
+interface ComposerForm {
+  content: FormControl;
+  image: FormControl;
+}
+
+enum BlockType {
+  text = 'TEXT',
+  images = 'IMAGE',
+}
 
 @Component({
   selector: 'app-composer',
   templateUrl: './composer.component.html',
 })
-export class ComposerComponent {
-  @Input() composerForm: FormGroup;
-  blockTypes = BlockType;
+export class ComposerComponent implements OnInit {
+  blockTypes: typeof BlockType = BlockType;
   initBlock = {
     type: BlockType.text,
     content: {
@@ -17,20 +26,75 @@ export class ComposerComponent {
     },
   };
 
-  blocksFormArray: FormArray = new FormArray([new FormControl(this.initBlock)]);
+  @Input() composerForm: FormGroup;
 
-  addBlock(block: BlockType) {
-    this.blocksFormArray.push(
-      new FormControl({
-        type: block,
-        content: {
-          value: '',
-        },
-      })
-    );
+  @Output() private readonly createBlog = new EventEmitter();
+
+  blocksFormArray: FormArray = new FormArray([new FormControl(this.initBlock)]);
+  ngOnInit(): void {
+    this.composerForm = new FormGroup<ComposerForm>({
+      content: new FormControl(),
+      image: new FormControl(),
+    });
+  }
+
+  postBlog(event: Event) {
+    console.log({ form: this.composerForm.getRawValue() });
+    this.createBlog.emit();
   }
 
   handleFileSelect(event: Event) {
-    const fileList = (event.target as HTMLInputElement).files;
+    const files = (event.target as HTMLInputElement).files;
+
+    this.handleFileDrop(files);
+  }
+
+  handleFileDrop(fileList: FileList) {
+    let files: File[] = [];
+    let errorMessage: string;
+    const maxFileSize = 2 * 1024 * 1024;
+    const acceptedImageFormat = ['image/jpg', 'image/png', 'image/jpeg'];
+
+    if (!fileList.length) {
+      console.log('image is empty');
+      return;
+    }
+
+    for (let i: number = 0; i < fileList.length; i++) {
+      const file: File = fileList[i];
+
+      if (!acceptedImageFormat.includes(file.type))
+        errorMessage = 'Image format should be in jpg, jpeg, png format';
+
+      if (file.size < maxFileSize) errorMessage = 'Image size is too large.';
+
+      // if (!errorMessage) files = [...files, file];
+      files = [...files, file];
+    }
+
+    const images = files.map((file: File) => ({
+      file,
+      url: URL.createObjectURL(file),
+    }));
+
+    console.log({ images });
+
+    const imagesControl = this.blocksFormArray.controls.find(
+      (control) => control.value.type === this.blockTypes.images
+    );
+
+    if (imagesControl) {
+      imagesControl.setValue({
+        type: this.blockTypes.images,
+        content: [...imagesControl.value.content, ...images],
+      });
+    } else {
+      this.blocksFormArray.push(
+        new FormControl({
+          type: this.blockTypes.images,
+          content: images,
+        })
+      );
+    }
   }
 }
